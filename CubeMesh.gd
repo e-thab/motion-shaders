31e6a5f5a14407c2428c7f1756d5eae5ccbd5a07
Ethan <1980554+Athen-H@users.noreply.github.com>
2024-08-27 17:05:07 -0400

    cleanup, resolution scale

--- CubeMesh.gd
+++ CubeMesh.gd
@@ -1,6 +1,6 @@
 extends MeshInstance3D
 
-var spin_axis : Vector3 = Vector3.UP
+var last_mouse_pos : Vector2 = Vector2.ZERO
 
 
 # Called when the node enters the scene tree for the first time.
@@ -10,28 +10,16 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	#match randi()%3:
-		#0:
-			#spin_axis = spin_axis.rotated(
-				#Vector3.UP,
-				#randf_range(0, PI/4)
-			#)
-		#1:
-			#spin_axis = spin_axis.rotated(
-				#Vector3.RIGHT,
-				#randf_range(0, PI/4)
-			#)
-		#2:
-			#spin_axis = spin_axis.rotated(
-				#Vector3.FORWARD,
-				#randf_range(0, PI/4)
-			#)
-			#
-	#rotate(spin_axis, delta*PI/8)
-	#spin_axis = spin_axis.rotated()
-	
-	#rotate(Vector3.UP, delta*PI/(randi()%8+1))
-	#rotate(Vector3.RIGHT, delta*PI/(randi()%8+1))
-	#rotate(Vector3.FORWARD, delta*PI/(randi()%8+1))
-	
-	rotate(Vector3.UP, delta*PI/8)
+	#rotate(Vector3.UP, delta*PI/4)
+	pass
+
+func _input(event):
+	# Mouse in viewport coordinates.
+	if event is InputEventMouseMotion:
+		var x_change = event.position.x - last_mouse_pos.x
+		var y_change = event.position.y - last_mouse_pos.y
+		
+		rotate(Vector3.UP, x_change/128)
+		rotate(Vector3.RIGHT, y_change/128)
+		
+		last_mouse_pos = event.position
--- main.gd
+++ main.gd
@@ -1,54 +1,34 @@
 extends Node3D
 
-@export var pov_shader : ShaderMaterial
-@export var simple_shader : ShaderMaterial
+@export var resolution_scale : float = 1.0
 
-@onready var overlay : TextureRect = $RenderVPContainer/RenderViewport/OverlayRect
-@onready var rod : MeshInstance3D = $StagingVPContainer/StagingViewport/RodMesh
-
-@onready var stagingView : SubViewportContainer = $StagingVPContainer
-@onready var renderView : SubViewportContainer = $RenderVPContainer
+@onready var rod : MeshInstance3D = $RodMesh
+@onready var stagingView : SubViewport = $StagingVPContainer/StagingViewport
+@onready var renderView : SubViewport = $RenderVPContainer/RenderViewport
 
 var time_elapsed = 0.0
 var frame_time = 2.0
 var snap = 0
 var paused = false
-var noise_texture
+
+var stage_tex
+var last_stage_tex
 
 # Wisdom: https://forum.gamemaker.io/index.php?threads/solved-issue-trying-to-imitate-visual-effect-with-shaders-and-surfaces.109391/
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# Initialize RNG
-	randomize()
+	#randomize()
 	
-	# Generate noise texture
-	noise_texture = ImageTexture.new()
-	var width = $StagingVPContainer.get_viewport_rect().size.x
-	var height = $StagingVPContainer.get_viewport_rect().size.y
-	print(width, "x", height)
-	var noise_img = Image.create(width, height, false, Image.FORMAT_RGB8)
+	## Apply resolution scale factor
+	get_tree().root.content_scale_factor = resolution_scale
+	renderView.size.x = int(stagingView.size.x / resolution_scale)
+	renderView.size.y = int(stagingView.size.y / resolution_scale)
+	stagingView.size.x = int(stagingView.size.x / resolution_scale)
+	stagingView.size.y = int(stagingView.size.y / resolution_scale)
 	
-	# Randomly populate texture with black/white pixels (noise)
-	for x in range(width):
-		for y in range(height):
-			if randi() % 2 == 0:
-				noise_img.set_pixel(x, y, Color.WHITE)
-			else:
-				noise_img.set_pixel(x, y, Color.BLACK)
-	
-	# Assign image to texture
-	noise_texture = ImageTexture.create_from_image(noise_img)
-	#RenderingServer.global_shader_parameter_set("noise", noise_texture)
-	RenderingServer.global_shader_parameter_set(
-		"last_frame",
-		noise_texture
-	)
-	overlay.texture = noise_texture
-	#noise_img.save_png("./out/noise.png")
-	
-	#print("Pixel (300, 300): ", noise_img.get_pixel(300, 300))
-	#print("Overlay texture size: ", overlay.texture.get_size())
+	get_snapshots()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -60,42 +40,47 @@ func _process(delta: float) -> void:
 		else:
 			paused = true
 			Engine.time_scale = 0.0
-		RenderingServer.global_shader_parameter_set("paused", paused)
 	
 	if Input.is_action_just_pressed("render_swap"):
-		if renderView.visible:
-			renderView.hide()
+		if $RenderVPContainer.visible:
+			$RenderVPContainer.hide()
 		else:
-			renderView.show()
-		#if overlay.material == pov_shader:
-			#overlay.material = simple_shader
-		#else:
-			#overlay.material = pov_shader
-		
+			$RenderVPContainer.show()
 	
 	## Oscillate the rod
 	rod.position.x = 3 * cos(time_elapsed*PI/6)
 	time_elapsed += delta
 	
-	
-	if time_elapsed < 0.5:
-		return
-	
-	## Take a snapshot of the current frame, store
-	var render_snapshot = renderView.get_viewport().get_texture().get_image()
-	var staging_snapshot = stagingView.get_viewport().get_texture().get_image()
-	
-	var snapshot_tex = ImageTexture.create_from_image(render_snapshot)
+	#frame_time += delta
+	get_snapshots()
+
+
+func get_snapshots():
+	## Take a snapshot of the current 'stage' frame, assign to global shader param
+	var staging_snapshot = stagingView.get_texture().get_image()
+	## Store last and second-to-last stage textures
+	last_stage_tex = stage_tex
+	stage_tex = ImageTexture.create_from_image(staging_snapshot)
 	RenderingServer.global_shader_parameter_set(
-		"last_frame",
-		snapshot_tex
+		"last_stage",
+		stage_tex
+	)
+	RenderingServer.global_shader_parameter_set(
+		"second_last_stage",
+		last_stage_tex
 	)
 	
-	## Sanity check: snapshot of last frame every 2 seconds
+	## Take a snapshot of the current 'render' frame, assign to global shader param
+	var render_snapshot = renderView.get_texture().get_image()
+	var render_tex = ImageTexture.create_from_image(render_snapshot)
+	RenderingServer.global_shader_parameter_set(
+		"last_render",
+		render_tex
+	)
+	
 	#if frame_time >= 2.0:
 		#print('Snapshot ' + str(snap))
 		#render_snapshot.save_png("./out/snap-" + str(snap) + "-R.png")
 		#staging_snapshot.save_png("./out/snap-" + str(snap) + "-S.png")
 		#frame_time = 0.0
 		#snap += 1
-	#frame_time += delta
